0a32129f9128069e88b87ddda62e9b4562cc49e4
Adem Polat <polatadem.2541@gmail.com>
2022-12-26 23:42:38 +0100

    onmouseover-out

--- Kendim_js/ders4_dom/ornek2_domManipule/app.js
+++ Kendim_js/ders4_dom/ornek2_domManipule/app.js
@@ -26,5 +26,48 @@ document.querySelector(".ekle").onclick=function(){
 }
 //sil butonuna tiklandiginda li elemani silinsin
 document.querySelector(".sil").onclick=function(){
-    liste.removeChild(liste.lastChild);
+    liste.removeChild(liste.lastElementChild);
+    //listenin cocuklarindan listenin son cocugunu sildik
+    
+}
+const parag=document.querySelector(".forH1");
+parag.innerHTML=parag.innerHTML+`<h1>${"Programlama Dilleri"}</h1>`;
+
+//--->=====Klavyeden textbox a basildiginda  buyusun kuculsun
+document.querySelector(".textbox").onkeyup=function(){ //text box'a klavyeden giris yapilirsa
+  const checkbox=document.querySelector(".checkbox");
+  const textbox=document.querySelector(".textbox");
+  if(checkbox.checked){
+    textbox.value=textbox.value.toUpperCase();
+
+  }else {
+    textbox.value=textbox.value.toLowerCase();
+
+  }
+}
+//mouse resmin ustune geldiginde aslan kukresin
+resim.onmouseover=function(){
+    resim.src="./img/aslan2.jpg";
+
+}
+//2. Yol(uzun) addEventListener() yolu
+//resim.addEventListener("mouseout",function(){
+   // resim.src="./img/aslan2.jpg";
+//})
+
+
+//mouse resmin ustunden cekildiginde
+resim.onmouseout=function(){
+    resim.src="./img/aslan1.jpg";
+
+}
+//enter=13 delete=46
+//onkeydown=klavyedeki tusa basip elimizi cektigimizde
+document.querySelector(".dil").onkeydown=function(klavye){
+  if(klavye.keyCode==13){//Enteri cagirdik
+
+document.querySelector(".ekle").onclick();
+  }if(klavye.keyCode==46){//Enteri cagirdik
+document.querySelector(".sil").onclick();
+  }
 }
