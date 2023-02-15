7facb3296492aa5727616c9a114e091a2abdaf3c
Adem Polat <polatadem.2541@gmail.com>
2023-02-15 16:50:09 +0100

    DIziler @

--- ders5_diziler/ornek1_diziMetodlari/diziler.js
+++ ders5_diziler/ornek1_diziMetodlari/diziler.js
@@ -50,5 +50,49 @@ meyveler.reverse();
 
 //sort() diziyi natural order alfabetik siralar
 meyveler.sort();
-baslik.textContent=meyveler;
-console.log(meyveler);
+
+
+//Splice() methodu
+meyveler.splice(1,0,"Erik"); //1. indexe erik yaz 0 kalsin hicbir eleman silinmesin
+meyveler.splice(4,1,"uzum");//4. indexe uzum yaz ordaki elemani sil===1 ve 0 true ve false gibi 
+//0 silme ve ekle 1 elemani sil ekle
+
+//******DIZI ERISIM METODLARI (Bunlar diziyi degistirmezler) */
+
+const sayilar=[3,4,5,2,"2","uc","bes",5,2,7];
+ console.log(sayilar.includes("5")); //false
+ console.log(sayilar.includes(5)); //true
+ //includes() kapsiyor mu
+
+ //indexOf() ilk eslesen index i dondurur
+ console.log(sayilar.indexOf(2));
+ console.log(sayilar.lastIndexOf(5));
+ console.log(sayilar.indexOf(2,4)); //2 elemanini 4. indexten sonra ara
+
+ //join() dizinin elemanlarini birlestirip string hale getirir
+ console.log(sayilar.join(" "));
+ console.log(sayilar.join("-"));
+ //join default olraka virguller ile ayiratak diziden kurtarir.Ama istersek " " yazarak bosluklarla
+ // "-" tirelerle ayirabiliriz
+
+
+ //toString join gibidir ama sadece virgul ile ayirir
+
+ console.log(sayilar.toString());
+
+ //********** */
+
+ //slice() dizinin 
+
+const arabalar=["bmw","mercedes","Volvo","sahin","anadol"];
+console.log(arabalar.slice(2)); // 2. indexli elemandan sonuna kadar
+
+console.log(arabalar.slice(1,3)); //ilk index dahil ,2.sirada yazan index e kadar (2.dahil deil)
+
+//*****CONCAT**** */
+const birlesik=sayilar.concat(arabalar);
+console.log(birlesik);
+
+
+baslik.textContent=sayilar;
+console.log(sayilar);
